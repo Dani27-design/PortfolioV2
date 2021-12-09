6e287ad941d94886cd8f10f81eed7da244d4ca5c
Dani27-design <muhammad.19043@mhs.unesa.ac.id>
2021-12-10 00:27:45 +0700

    fixing bug carousel

--- src/Components/Certification.jsx
+++ src/Components/Certification.jsx
@@ -1,25 +1,70 @@
-import React from 'react'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Sertif1 from '../Assets/sertif1.png'
+import Sertif2 from '../Assets/sertif2.png'
+import Sertif3 from '../Assets/sertif3.png'
+import Sertif4 from '../Assets/sertif4.png'
+import Sertif5 from '../Assets/sertif5.png'
+import Sertif6 from '../Assets/sertif6.png'
+import Sertif7 from '../Assets/sertif7.png'
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+
+const images = [Sertif1, Sertif2, Sertif3, Sertif4, Sertif5, Sertif6, Sertif7]
 
 function Certification() {
+    const [imageIndex, setImageIndex] = useState(0)
+
+    const NextArrow = ({ onClick }) => {
+        return (
+            <div className='next' onClick={onClick}></div>
+        )
+    };
+
+    const PrevArrow = ({ onClick }) => {
+        return (
+            <div className='prev' onClick={onClick}></div>
+        )
+    };
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        lazyLoad: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: 0,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        beforeChange: (current, next) => setImageIndex(next),
+        customPaging: function (i) {
+            return (
+                <a>
+                    <div className={i === imageIndex ? "dots-slider-active" : "dots-slider"}></div>
+                </a>
+            );
+        },
+    };
     return (
         <div id="certification">
             <div className="container-certification">
-                <Carousel dynamicHeight={true} showStatus={false}>
-                    <div>
-                        <img className="img" src={`${process.env.PUBLIC_URL}/sertif1.png`} alt="" />
-                        <p className="legend">Legend 1</p>
-                    </div>
-                    <div>
-                        <img className="img" src={`${process.env.PUBLIC_URL}/sertif2.png`} alt="" />
-                        <p className="legend">Legend 2</p>
-                    </div>
-                    <div>
-                        <img className="img" src={`${process.env.PUBLIC_URL}/sertif3.png`} alt="" />
-                        <p className="legend">Legend 3</p>
-                    </div>
-                </Carousel>
+                <div className="container-header-certification">
+                    <p className="header-certification">Certification</p>
+                    <p className="teks-certification">This is my achievement</p>
+                </div>
+                <Slider {...settings}>
+                    {images.map((img, idx) => (
+                        <div className={idx === imageIndex ? "slide activeSlide" : "slide"}>
+                            <Zoom zoomMargin={40}>
+                                <img src={img} alt={img} />
+                            </Zoom>
+                        </div>
+                    ))}
+                </Slider>
             </div>
         </div>
     )
