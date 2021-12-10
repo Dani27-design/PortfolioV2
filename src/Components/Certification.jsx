b00d4523ae94bf8c93deeb15bc11ad3b7e81819f
Dani27-design <muhammad.19043@mhs.unesa.ac.id>
2021-12-10 13:06:42 +0700

    update certificate

--- src/Components/Certification.jsx
+++ src/Components/Certification.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -9,10 +9,36 @@ import Sertif4 from '../Assets/sertif4.png'
 import Sertif5 from '../Assets/sertif5.png'
 import Sertif6 from '../Assets/sertif6.png'
 import Sertif7 from '../Assets/sertif7.png'
+import Sertif8 from '../Assets/sertif8.png'
+import Sertif9 from '../Assets/sertif9.png'
+import Sertif10 from '../Assets/sertif10.png'
+import Sertif11 from '../Assets/sertif11.png'
+import Sertif12 from '../Assets/sertif12.png'
+import Sertif13 from '../Assets/sertif13.png'
+import Sertif14 from '../Assets/sertif14.png'
+import Sertif15 from '../Assets/sertif15.png'
+import Sertif16 from '../Assets/sertif16.png'
 import Zoom from 'react-medium-image-zoom'
 import 'react-medium-image-zoom/dist/styles.css'
 
-const images = [Sertif1, Sertif2, Sertif3, Sertif4, Sertif5, Sertif6, Sertif7]
+const images = [
+    Sertif1,
+    Sertif2,
+    Sertif3,
+    Sertif4,
+    Sertif5,
+    Sertif6,
+    Sertif7,
+    Sertif8,
+    Sertif9,
+    Sertif10,
+    Sertif11,
+    Sertif12,
+    Sertif13,
+    Sertif14,
+    Sertif15,
+    Sertif16,
+]
 
 function Certification() {
     const [imageIndex, setImageIndex] = useState(0)
@@ -51,6 +77,7 @@ function Certification() {
             );
         },
     };
+
     return (
         <div id="certification">
             <div className="container-certification">
@@ -62,7 +89,7 @@ function Certification() {
                     {images.map((img, idx) => (
                         <div className={idx === imageIndex ? "slide activeSlide" : "slide"}>
                             <Zoom zoomMargin={40}>
-                                <img src={img} alt={img} />
+                                <img src={img} alt={img} width={300} />
                             </Zoom>
                         </div>
                     ))}
