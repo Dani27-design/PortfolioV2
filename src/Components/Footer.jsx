48738cfca08fbfce8a7108c82ef06ddbffe6141c
Dani27-design <muhammad.19043@mhs.unesa.ac.id>
2021-12-10 17:49:47 +0700

    done web version

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Footer() {
     return (
         <div id="footer">
-            <p>DCopyright2022</p>
+            <div className="container-logo">
+                <p className="logo-footer">DC<img className="dot-navbar" src={`${process.env.PUBLIC_URL}/dot-navbar.png`} alt="" /></p>
+                <p className='copyright'>copyright2022</p>
+            </div>
+            <div className="container-build">
+                <p className='teks-build'>Build with </p>
+                <img src={`${process.env.PUBLIC_URL}/logo.svg`} alt="" className="logo-react" />
+            </div>
         </div>
     )
 }
